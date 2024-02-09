d94c90309e11790da34df47dd8c8991ae3e07719
yannick <yan_1731@hotmail.com>
2024-02-09 10:51:45 -0500

    Add player, bed and billboard scene

---
+++ scenes/Player/Player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("primary action"):
+		print("pew pew")
+	if Input.is_action_just_pressed("secondary action"):
+		print("BOOM")
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * SPEED;
+	move_and_slide()
