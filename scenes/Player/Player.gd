3d673840bda78d81830d4ceac7b64727b07ab8d8
yannick st-laurent <code@yann1731.com>
2024-02-11 16:10:57 -0500

    Add drone enemy and new level

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -7,7 +7,7 @@ const SPEED = 300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if Input.is_action_just_pressed("primary action"):
 		print("pew pew")
 	if Input.is_action_just_pressed("secondary action"):
